8314a9d223dfdf9439b1b3d23439c7bbfb7d743f
Chris Ehrman <cehr@loc.gov>
2018-01-23 10:20:13 -0500

    Create squatter_sovereign.py

---
+++ squatter_sovereign.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import json
+import urllib
+
+# Enter Chronicling Ameirca JSON search results url
+results = 'http://chroniclingamerica.loc.gov/search/pages/results/?lccn=sn82015827&dateFilterType=yearRange&date1=1789&date2=1949&language=&ortext=&andtext=&phrasetext=&proxtext=&proxdistance=5&rows=500&searchType=advanced&format=json'
+
+# Count to keep track of number of files downloaded
+count = 0
+
+def get_json(url):
+	data = urllib.urlopen(url).read()
+	return json.loads(data)
+
+data = get_json(results)
+
+# Cycle through JSON results
+for page in data['items']:
+	
+	# Create URL
+	hit = str(page['id'].encode('utf-8'))
+	seed = hit[:-1] + '.pdf'
+	download_url = chronam + seed
+	
+	# Create File Name
+	file_name = download_url.replace('/', '_')
+	file_name = file_name[39:] 
+
+	# Download and save PDF 
+	response = urllib.urlopen(download_url)
+	file = open(str(file_name), 'wb')
+	file.write(response.read())
+	file.close()
+	print 'file saved: ' + file_name
+	count += 1
+
+# Prints number of PDFs downloaded
+print str(count) + ' results downloaded'
